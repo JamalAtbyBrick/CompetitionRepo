eabc1ca384ee78ae9a883e97ea5a352a8c2d0e92
Brian H. Madsen <sub.lists@outlook.com>
2022-12-05 14:02:30 +0100

    Writeth the codeth

---
+++ FixMe/Options/RunnerOptions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FixMe.Options
+{
+    public class RunnerOptions
+    {
+        /// <summary>
+        /// The number of iterations our process is going to run;
+        /// </summary>
+        public int Iterations { get;} = 500000;
+
+        /// <summary>
+        /// The filepath that we want the cheat sheet to be printed to.
+        /// </summary>
+        public string FilePath { get; } = "./exam-cheat-sheet.cs";
+
+    }
+}
---
+++ FixMe/Runners/RunFaster.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BenchmarkDotNet.Attributes;
+using FixMe.Entities;
+using FixMe.Options;
+
+namespace FixMe.Runners
+{
+    public class RunFaster
+    {
+        private readonly RunnerOptions options;
+
+        private object[] _all_the_irckles;
+
+        public RunFaster()
+        {
+            options = new RunnerOptions();
+            _all_the_irckles = Fill.FillDataObjectArray();
+        }
+        
+        [Benchmark]
+        public void Run() 
+        {               
+            using (StreamWriter sw = File.CreateText(options.FilePath))
+            {
+                foreach (var irckle in _all_the_irckles)
+                {
+                    if (irckle is Cuirckle)
+                    {
+                        var _irckle = irckle as Cuirckle;
+                        double circumference = _irckle.Calculate;
+                        sw.WriteLine($"Circumference of circle: {_irckle.RandomRadius}:{circumference}");
+                        sw.Flush();
+                    }
+                    else
+                    {
+                        var _angle = irckle as Atrangle;
+                        double area = _angle.Calculate;
+                        sw.WriteLine($"Area of triangle: {area}");
+                        sw.Flush();
+                    }                    
+                }               
+            }
+        }
+    }
+}
